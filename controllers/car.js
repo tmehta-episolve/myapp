431c21089bb0865859c03d54b7584e516ff23d0c
tmehta-episolve <tmehta@episolve.net>
2013-04-24 18:55:43 +0530

    Add/Edit/Delete Car Page

--- controllers/car.js
+++ controllers/car.js
@@ -16,26 +16,51 @@ exports.add = function(req,res){
 }
 
 exports.adddata = function (req, res) {
-// var cars = new Car({car_name:req.body.car_name});
-/*var cars = new Car(); 
-cars.save({
-car_name: req.param('car_name'),
-color: req.param('color')
-//company: req.company('company')
-}, function( error, docs) {
-        res.redirect('/')
-    }
-);
-*/
-var cars= new Car(req.param);
-//res.send(req.param('car_name'));
-  
-  cars.save(function (err) {
+
+ var car = new Car({
+   car_name: req.param('car_name'),
+   color: req.param('color'),
+   company: req.param('company')
+ });
+  car.save(function (err) {
     if(err) {
       console.log(err)
     } else {
       res.redirect('/cars');
-      console.log(cars);
+      console.log(car);
     }
   });
+}
+// display template and data in template
+exports.edit = function(req,res){
+  Car.findOne({_id : req.params.id}, function(err, cars) {
+    res.render('car_edit',
+       {
+      title: 'Edit Car',
+      car_name:cars.car_name,
+      color:cars.color,
+      company:cars.company,
+      _id:cars._id
+       });
+ });
+}
+// save data at time of edit
+exports.editdata = function (req, res) {
+//res.send(req.body);
+  Car.findOneAndUpdate({_id : req.body.id}, req.body,
+
+  function(err, cars) {
+      console.log(cars);
+    res.redirect('/cars/');
+  });
+}
+exports.remove = function(req,res){
+//res.send(req.param('id'));
+ Car.remove({_id : req.param('id')},
+
+  function(err, cars) {
+      console.log(cars);
+    res.redirect('/cars/');
+  });
+
 }
--- routes.js
+++ routes.js
@@ -4,6 +4,9 @@ var phonebook= require('./controllers/phonebook');
   app.get('/cars', cars.list);
   app.get('/cars/add',cars.add);
   app.get('/cars/adddata',cars.adddata);
+  app.get('/cars/:id/edit',cars.edit);
+  app.post('/cars/editdata',cars.editdata);
+  app.get('/cars/:id/delete',cars.remove);
 //  app.get('/phonebook',phonebook.list);
 //  app.post('/phonebook/add', phonebook.add);
 }
